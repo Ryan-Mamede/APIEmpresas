5f279bfd79d33a945523697f3197c6b8029fc887
Ryan Mamede <85031572+Ryan-Mamede@users.noreply.github.com>
2025-06-08 09:36:12 -0300

    Update SwaggerConfiguration.cs

--- ApiEmpresas.Services/Configurations/SwaggerConfiguration.cs
+++ ApiEmpresas.Services/Configurations/SwaggerConfiguration.cs
@@ -21,8 +21,8 @@ namespace ApiEmpresas.Services.Configurations
                     Description = "Projeto desenvolvido em NET6 API com EntityFramework SqlServer",
                     Contact = new OpenApiContact
                     {
-                        Name = "COTI Informática",
-                        Url = new Uri("http://www.cotiinformatica.com.br"),
+                        Name = "Ryan Mamede",
+                        Url = new Uri("https://www.linkedin.com/in/ryan-mamede-799b141aa/"),
                         Email = "contato@cotiinformatica.com.br"
                     }
                 });
